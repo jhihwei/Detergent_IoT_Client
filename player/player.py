8ffb56b52108e5bb6fa6fd2b8327ad317b9b6b9d
Your Name <you@example.com>
2020-03-24 20:06:32 +0800

    init

--- player/player.py
+++ player/player.py
@@ -16,7 +16,7 @@ player.set_playback_mode(vlc.PlaybackMode.loop)
 system_path = os.path.abspath(os.getcwd())
 @app.route("/play")
 def play():
-    test_videos_path = system_path+'test_videos/'
+    test_videos_path = system_path+'/test_videos/'
     files = os.listdir(test_videos_path)
     media_list = instance.media_list_new()
     for f in files:
