1377eec725706e0a3d4cf896e4f6779c7c111bdb
Your Name <you@example.com>
2020-04-25 22:54:18 +0800

    增加VLC選項

--- player2/player.py
+++ player2/player.py
@@ -1,19 +1,24 @@
 import subprocess
 import os
-
+from dotenv import load_dotenv
 
 class Player2:
     def __init__(self):
+        load_dotenv(dotenv_path="/home/pi/Detergent_IoT_Client/")
+        self._PLAYER = str(os.getenv('PLAYER'))
         self.system_path = os.path.dirname(os.path.abspath(__file__))
         self.videos_path = os.path.join(
             self.system_path, "..", "player", "test_videos")
-        print()
+        print(self._PLAYER)
 
     def play(self):
         files = os.listdir(self.videos_path)
         for f in files:
             f = f'{self.videos_path}/{f}'
-            subprocess.run(["omxplayer", "-o", "hdmi", f])
+            if self._PLAYER == "vlc":
+                subprocess.run(["vlc", "--fullscreen", f])
+            else:
+                subprocess.run(["omxplayer", "-o", "hdmi", f])
 
 
 if __name__ == '__main__':
