1b3d2202cb3bf9a60d03ee177daac99ad2793f9b
Your Name <you@example.com>
2020-04-26 21:22:21 +0800

    .

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -44,4 +44,5 @@ if __name__ == "__main__":
     img_base64 = monitor.convert_img_to_base64('output.jpg')
     m = Mqtt_Controller()
     m.set_TOPIC('screenshot')
+    m.publish(img_base64)
     print(m.get_TOPIC())
