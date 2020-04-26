acf88f03bf7ea5e8eb216755509c59b091f00867
Your Name <you@example.com>
2020-04-26 21:01:29 +0800

    .

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -15,6 +15,7 @@ load_dotenv(dotenv_path="/home/pi/Detergent_IoT_Client/.env")
 # --------------------------------------
 import subprocess
 import cv2
+import base64
 class Monitor:
     def __init__(self):
         pass
@@ -22,12 +23,17 @@ class Monitor:
         subprocess.run(["raspi2png"])
         img = cv2.imread("snapshot.png")
         h, w, channels = img.shape
+        # resize img and save it
         ratio = 320
         h_ratio = h/ratio
         h = int(h/h_ratio)
         w = int(w/h_ratio)
         img = cv2.resize(img, (w, h), interpolation=cv2.INTER_CUBIC)
         cv2.imwrite('output.jpg', img)
+    def convert_img_to_base64(self, image_path:str):
+        with open(f'{image_path}', "rb") as img_file:
+            img_base64 = base64.b64encode(img_file.read())
+            print(img_base64)
 if __name__ == "__main__":
     monitor = Monitor()
     monitor.take_screenshot()
