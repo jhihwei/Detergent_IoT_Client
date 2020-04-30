81d7e42db230476766635087b919baf87421c440
Your Name <you@example.com>
2020-04-30 21:12:30 +0800

    .

--- install.sh
+++ install.sh
@@ -1,18 +1,65 @@
-# sudo apt-get install -y libatlas-base-dev
-# sudo apt-get install -y libjasper-dev
-# sudo apt-get install -y libqtgui4
-# sudo apt-get install -y python3-pyqt5
-# sudo apt install -y libqt4-test
-# curl -sL https://raw.githubusercontent.com/AndrewFromMelbourne/raspi2png/master/installer.sh | bash -
-sudo echo -e "[Unit]
-  Description=Screen Monitor \n
-  [Service] \n
-  User=pi \n
-  WorkingDirectory=/home/pi/Detergent_IoT_Client/monitor/ \n
-  ExecStart=/usr/bin/python3 /home/pi/Detergent_IoT_Client/monitor/monitor.py \n
-  Type=simple \n
-  RemainAfterExit=yes \n
-  [Install] \n
-  WantedBy=multi-user.target" >> /etc/systemd/system/test.service 
-sudo systemctl enable test.service
-sudo systemctl restart test.service
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y libjasper-dev
+sudo apt-get install -y libqtgui4
+sudo apt-get install -y python3-pyqt5
+sudo apt install -y libqt4-test
+curl -sL https://raw.githubusercontent.com/AndrewFromMelbourne/raspi2png/master/installer.sh | bash -
+
+mkdir ~/Detergent_IoT_Client
+
+cd ~/Detergent_IoT_Client
+
+git clone https://github.com/jhihwei/Detergent_IoT_Client.git
+
+pip3 install -r requirements.txt
+
+sudo echo -e 
+"[Unit]
+    Description=Video Player
+    [Service]
+    User=pi
+    ExecStart=/usr/bin/python3 /home/pi/Detergent_IoT_Client/player2/player.py
+    Type=simple
+    RemainAfterExit=yes
+    [Install]
+    WantedBy=multi-user.target" >> /etc/systemd/system/player.service
+sudo systemctl enable player.service
+sudo systemctl restart player.service
+
+sudo echo -e 
+"[Unit]
+    Description=RS232 Controller
+    [Service]
+    User=pi
+    ExecStart=/usr/bin/python3 /home/pi/Detergent_IoT_Client/RS232/Rs232_Controller.py
+    Type=simple
+    RemainAfterExit=yes
+    [Install]
+    WantedBy=multi-user.target" >> /etc/systemd/system/RS232_Controller.service
+sudo systemctl enable RS232_Controller.service
+sudo systemctl restart RS232_Controller.service
+
+sudo echo -e 
+"[Unit]
+  Description=Screen Monitor
+  [Service]
+  User=pi
+  WorkingDirectory=/home/pi/Detergent_IoT_Client/monitor/
+  ExecStart=/usr/bin/python3 /home/pi/Detergent_IoT_Client/monitor/monitor.py
+  Type=simple
+  RemainAfterExit=yes
+  [Install]
+  WantedBy=multi-user.target" >> /etc/systemd/system/Screen_Monitor.service
+sudo systemctl enable Screen_Monitor.service
+sudo systemctl restart Screen_Monitor.service
+
+sudo echo -e 
+"[Unit]
+  Description=Auto Key
+  [Service]
+  User=root
+  ExecStart=sudo /usr/bin/python3 /home/pi/Detergent_IoT_Client/monitor/keyboard.py
+  Type=simple
+  RemainAfterExit=yes
+  [Install]
+  WantedBy=multi-user.target"
