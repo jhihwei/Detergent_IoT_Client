b33fafbdf694205f92f7dbd2d373b2a3dc3a9bb7
Your Name <you@example.com>
2020-05-01 15:54:02 +0800

    .

--- RS232/Rs232_Controller.py
+++ RS232/Rs232_Controller.py
@@ -1,17 +1,16 @@
-# Dot ENV 預載模組-----------------------
-from struct import *
-from datetime import datetime
-import paho.mqtt.client as mqtt
-from dotenv import load_dotenv
-import os
-load_dotenv()
-# --------------------------------------
 # 增加系統路徑---------------------------
 import sys
 dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 libs_dir_path = parent_dir_path+'/libs'
 sys.path.insert(0, libs_dir_path)
+# --------------------------------------
+# Dot ENV 預載模組-----------------------
+from struct import *
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # --------------------------------------
 from Mqtt_Controller import Mqtt_Controller
 import time
