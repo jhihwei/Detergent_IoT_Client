bfc38231af1182a9884ab9ba24df722d830bfb02
Your Name <you@example.com>
2020-05-17 13:54:09 +0800

    .

---
+++ RS232/test.py
@@ -0,0 +1,12 @@
+import serial
+ser = serial.Serial(
+            port=str(os.getenv('SERIAL_PORT')),
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
+while True:
+    x = ser.read()
+    print(x)
