61e0a4898b14ed23d86cab2bd9d2e52798b99008
Your Name <you@example.com>
2020-05-17 14:28:40 +0800

    .

--- RS232/Rs232_Controller.py
+++ RS232/Rs232_Controller.py
@@ -44,9 +44,9 @@ class Recevier():
                 print(data)
                 d = data.split(',')
                 # 最後一個為[]，倒數第二個為chksum，。如果serial讀取有誤chksum為xx
-                chksum = 'xx'
-                if len(d[-2]) > 0:
-                    chksum = d[-2]
+                chksum = d[-2]
+                # if len(d[-2]) > 0:
+                #     chksum = d[-2]
                 # 由0至倒數第三個(不含第三個)為資料
                 d = d[:-3]
                 if int(chksum, 16) == int(self.checksum(d), 16):
