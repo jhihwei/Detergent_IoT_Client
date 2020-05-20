a11610090d86a8be215757bbb9606d887da59afc
Your Name <you@example.com>
2020-05-20 12:00:20 +0800

    .

--- RS232/Rs232_Controller.py
+++ RS232/Rs232_Controller.py
@@ -57,7 +57,7 @@ class Recevier():
                         now = now.strftime("%m/%d/%Y,%H:%M:%S")
                         self.m.publish(self.m.get_TOPIC(), now, data)
                         data = 'fa,'
-
+                        print(self.check_income(income))
                         if self.check_income(income):
                             self.income = income
                             self.m.publish(self.m.get_TOPIC_2, now, data)
